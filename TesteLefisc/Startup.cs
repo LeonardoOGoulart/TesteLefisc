e86772320485f9802f58f2d7da26579505f5913d
Leonardo Goulart <leonardooliveiragoulart@gmail.com>
2021-08-17 14:45:02 -0300

    Migração DB

--- TesteLefisc/Startup.cs
+++ TesteLefisc/Startup.cs
@@ -37,7 +37,8 @@ namespace TesteLefisc
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<TesteLefiscContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("TesteLefiscContext")));
+                    options.UseMySql(Configuration.GetConnectionString("TesteLefiscContext"), builder =>
+builder.MigrationsAssembly("TesteLefisc")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
