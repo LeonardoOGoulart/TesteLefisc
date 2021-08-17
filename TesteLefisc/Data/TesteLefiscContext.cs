5de279ae3429d5e19438310dba411efdfb16a357
Leonardo Goulart <leonardooliveiragoulart@gmail.com>
2021-08-17 11:46:19 -0300

    CRUD Scaffolding

---
+++ TesteLefisc/Data/TesteLefiscContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TesteLefisc.Models;
+
+namespace TesteLefisc.Data
+{
+    public class TesteLefiscContext : DbContext
+    {
+        public TesteLefiscContext (DbContextOptions<TesteLefiscContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TesteLefisc.Models.Fornecedores> Fornecedores { get; set; }
+    }
+}
--- TesteLefisc/Startup.cs
+++ TesteLefisc/Startup.cs
@@ -9,6 +9,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TesteLefisc.Data;
 
 namespace TesteLefisc
 {
@@ -33,6 +35,9 @@ namespace TesteLefisc
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<TesteLefiscContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("TesteLefiscContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
